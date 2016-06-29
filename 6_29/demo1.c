82e9c0b92a3e093cf2f9fde71f0095d9b3374282
fanbrightup <fanbright@126.com>
2016-06-29 20:30:54 +0800

    对demo1.c的一些修改

--- 6_29/demo1.c
+++ 6_29/demo1.c
@@ -4,10 +4,10 @@
 int main()
 {
    printf("float 存储最大字节数 : %lu \n", sizeof(float));
-   printf("float 最小值: %E\n", FLT_MIN );
    printf("float 最大值: %E\n", FLT_MAX );
-   printf("精度值: %d\n", FLT_DIG );
+  //  printf("精度值: %d\n", FLT_DIG );
    printf("hello");
-
-   return 0;
+// /   return 0;
+  printf("float 最小值: %E\n", FLT_MIN );
+  return 0;
 }
