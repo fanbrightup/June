8568dc65993e13ae1c1315ef769df7d60006e1f2
fanbrightup <fanbright@126.com>
2016-06-30 12:36:23 +0800

    打印循环

--- 6_30/T1.java
+++ 6_30/T1.java
@@ -1,5 +1,9 @@
 public class T1{
    public static void main(String[] args) {
       System.out.println("hello");//  打印hello
+      int i = 0;
+      for(;i<100;i++){
+        System.out.println(i);
+      }
   }
 }
